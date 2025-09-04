0c51cea9e07a3807455f5c25a4ba0e14f2953866
kemely2021 <castillocaccirekemely@gmail.com>
2025-09-04 17:12:01 -0500

    Ejercicio 4: Número positivo, negativo o cero

--- 01_introduccion_python/ejercicio4.py
+++ 01_introduccion_python/ejercicio4.py
@@ -1,3 +1,11 @@
 # Ejercicio 4: Número positivo, negativo o cero
 # - Pide un número al usuario
 # - Indica si es positivo, negativo o cero
+
+n = float(input("Numero:"))
+if (n > 0):
+	print("Numero positivo")
+elif (n < 0):
+	print("Numero negativo")
+else:
+	print("Cero")
